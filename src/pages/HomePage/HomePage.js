92cd1356dbd1a23cf8d426d12748ac535ec4a77f
thangduongnfq <thang.duon@nfq.asia>
2022-06-06 16:06:06 +0700

    update homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  let navigator = useNavigate();
+  return (
+    <div className="page-container">
+      <div>
+        <p className="textfill">Home</p>
+        <div
+          className="goBack"
+          onClick={() => {
+            console.log('hello')
+            navigator("/Dashboard");
+          }}
+        >
+          <span> Go back to Dashboard </span>
+          <i className="fa-solid fa-arrow-left"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
